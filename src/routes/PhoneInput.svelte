7e4f34a48e8f7a0e2bac84b155fddf9b788c1650
Jessica Ho <hojessica1230@gmail.com>
2023-04-20 08:59:59 +0000

    clean up

--- src/routes/PhoneInput.svelte
+++ src/routes/PhoneInput.svelte
@@ -22,15 +22,6 @@
 	});
 </script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.css"
-	/>
-	<script
-		src="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/intlTelInput.min.js"
-	></script>
-</svelte:head>
 <input type="tel" id="phone" name="phone" />
 
 <style>
