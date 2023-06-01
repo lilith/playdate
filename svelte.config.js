433795ca0fb66ac3dc6862859c662075562b7222
Lilith River <lilith@imazen.io>
2023-06-01 12:45:42 -0600

    Add 'yarn nodebuild' for trad node deployment modes.

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import auto_adapter from '@sveltejs/adapter-auto'; // for serverless
+import node_adapter from '@sveltejs/adapter-node'; // for node server generation
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+console.log("process.env.NODE_MODE_ADAPTER=", process.env.NODE_MODE_ADAPTER);
+const use_node_adapter = !!process.env.NODE_MODE_ADAPTER;
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: use_node_adapter ? node_adapter() : auto_adapter()
 	},
 	preprocess: [vitePreprocess()]
 };
