166fcd28369b272ad5eb0c1c75ed0c476790e4ff
Lilith River <lilith@imazen.io>
2023-06-13 00:22:13 -0600

    Change logic for hiding login links

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -137,13 +137,13 @@ export async function POST({ request }: { request: Request }) {
 
 	// It's a security issue to share the auth link with the client. Don't do this if 
 	// import.meta.env.PROD OR public_env.PUBLIC_URL are set
-	if (import.meta.env.PROD || public_env.PUBLIC_URL) {
+	if (import.meta.env.DEV && !public_env.PUBLIC_URL){
+		return json(message);
+	} else {
 		return new Response(null,
 			{
 				status: 200
 			}
 		);
-	}else{
-		return json(message);
 	}
 }
