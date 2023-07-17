71ead5c1bce158a31dea162d04c0a0903875b7dc
Jessica Ho <hojessica1230@gmail.com>
2023-07-17 06:36:07 +0000

    hamburger nav

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import DownIcon from '../Icons/downIcon.svelte';
 	import { page } from '$app/stores';
+	import HamburgerIcon from '../Icons/hamburgerIcon.svelte';
+	import CloseIcon from '../Icons/closeIcon.svelte';
 
 	let show = false;
 	const path = $page.route.id ?? '';
@@ -16,63 +17,100 @@
 </script>
 
 <div>
-	<div
-		class="centered"
-		style="gap: 5px;"
-		on:click={() => (show = !show)}
-		on:keyup={() => (show = !show)}
-	>
-		<h1>{pageName}</h1>
-		<DownIcon />
+	<div id="hamburger" on:click={() => (show = !show)} on:keyup={() => (show = !show)}>
+		<HamburgerIcon />
 	</div>
+	<div class="centered" style="gap: 5px;">
+		<h1>{pageName}</h1>
+	</div>
+
 	{#key show}
-		<div id="navbar" class={show ? 'open' : ''}>
-			{#if !path.includes('profile')}
-				<a href="/profile">Profile</a>
-			{/if}
-			{#if !path.includes('household') && user.firstName}
-				<a href="/household">Household</a>
-			{/if}
-			{#if !path.includes('invites') && user.householdId}
-				<a href="/invites">Invites</a>
-			{/if}
-			{#if !path.includes('calendar') && user.householdId}
-				<a href="/calendar">Calendar</a>
-			{/if}
-			{#if !path.includes('circle') && user.householdId}
-				<a href="/circle">Circle</a>
-			{/if}
-			{#if !path.includes('dashboard') && user.householdId}
-				<a href="/dashboard">Dashboard</a>
-			{/if}
-			<button on:click={logout}>Log out</button>
+		<div class="overlay" class:overlay-open={show} />
+		<div class="menu right" class:menu-open={show}>
+			<div on:click={() => (show = !show)} on:keyup={() => (show = !show)} class="p-4">
+				<CloseIcon />
+			</div>
+			<div class="menu-contents right">
+				{#if !path.includes('profile')}
+					<a href="/profile">Profile</a>
+				{/if}
+				{#if !path.includes('household') && user.firstName}
+					<a href="/household">Household</a>
+				{/if}
+				{#if !path.includes('invites') && user.householdId}
+					<a href="/invites">Invites</a>
+				{/if}
+				{#if !path.includes('calendar') && user.householdId}
+					<a href="/calendar">Calendar</a>
+				{/if}
+				{#if !path.includes('circle') && user.householdId}
+					<a href="/circle">Circle</a>
+				{/if}
+				{#if !path.includes('dashboard') && user.householdId}
+					<a href="/dashboard">Dashboard</a>
+				{/if}
+				<button on:click={logout} class="right">Log out</button>
+			</div>
 		</div>
 	{/key}
 </div>
 
 <style>
-	#navbar {
-		text-align: right;
-		margin: auto;
-		background: white;
-		width: 100px;
-		border-radius: 5px;
-		overflow: hidden;
-		height: 0;
+	.p-4 {
+		padding: 1rem;
 	}
-	#navbar * {
-		padding: 0.1rem 0;
+	.menu-contents > :nth-child(odd) {
+		background-color: #ddd;
+	}
+	.menu-contents {
+		width: 100%;
+	}
+	.menu-contents > * {
+		width: 100%;
+		padding: 1rem;
 		font-size: large;
 		color: #5a5a5a;
+	}
+	.overlay {
+		position: fixed;
+		top: 0;
+		left: 0;
+		opacity: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5);
+		pointer-events: none;
+	}
+	.overlay-open {
+		opacity: 100%;
+	}
+
+	#hamburger {
+		display: flex;
+		justify-content: flex-end;
+	}
+	.menu {
+		position: fixed;
+		top: 0;
+		right: 0;
+		width: 50%;
+		height: 100vh;
+		text-align: right;
+		background-color: #f2f2f2;
+		transform: translateX(100%);
+	}
+	.right {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+	}
+
+	.menu-open {
+		transform: translateX(0);
 	}
 	.centered {
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
-
-	#navbar.open {
-		height: fit-content;
-		padding: 0.5rem;
 	}
 </style>
