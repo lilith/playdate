a624099fee05ae2903d1da6c482f3739e6f58739
Lilith River <lilith@imazen.io>
2023-07-31 20:48:01 -0600

    Update +page.ts to not require PUBLIC_URL

--- src/routes/profile/+page.ts
+++ src/routes/profile/+page.ts
@@ -1,6 +1,5 @@
 import type { PageLoad } from './$types';
 import { LANGUAGES } from '../../constants';
-import { PUBLIC_URL } from '$env/static/public';
 import { marked } from 'marked';
 
 export const load = (async ({ fetch }) => {
@@ -8,7 +7,6 @@ export const load = (async ({ fetch }) => {
 	const termsText = await res.text();
 	return {
 		LANGUAGES,
-		PUBLIC_URL,
 		terms: marked.parse(termsText, {
 			mangle: false,
 			headerIds: false
