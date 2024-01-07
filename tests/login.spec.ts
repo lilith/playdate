0815abc971f310e6292e7c7afd35c7c179a97ce4
Jessica Ho <hojessica1230@gmail.com>
2024-01-07 05:48:33 +0000

    fix login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,8 +8,8 @@ test.beforeEach(async () => {
 });
 
 test('Redirect to login page w/ prefilled phone num on expired magic link', async ({ page }) => {
-	await page.goto('http://localhost:5173/login/12015550121/3e99472f1003794c');
+	await page.goto('http://localhost:5173/login/3e99472f1003794c');
 
-	await page.waitForURL(`${host}?phone=12015550121&status=403`, { waitUntil: 'networkidle' });
-	await expect(page).toHaveURL(`${host}?phone=12015550121&status=403`);
+	await page.waitForURL(`${host}?phone=+12015550121&status=403`, { waitUntil: 'networkidle' });
+	await expect(page).toHaveURL(`${host}?phone=+12015550121&status=403`);
 });
